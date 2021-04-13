cf31b96da285f097497fdd07c53d5cd1a794d0a0
Abhishek-701 <walvekarabhishek701@gmail.com>
2021-04-13 14:13:35 +0530

    Database models updated

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Products
+from .models import *
 # Register your models here.
 
-admin.site.register(Products)
+admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(Orderitem)
+admin.site.register(ShippingAddress)
