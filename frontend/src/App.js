df338e906fd0705a5802517d52f96f655d7ed059
Abhishek-701 <walvekarabhishek701@gmail.com>
2021-04-22 17:43:36 +0530

    Login page created (Registration incomplete)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './Components/Footer'
 import HomeScreen from './Screens/HomeScreen'
 import ProductScreen from './Screens/ProductScreen'
 import CartScreen from './Screens/CartScreen'
+import LoginScreen from './Screens/LoginScreen'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <main className="py-3">
         <Container >
           <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen} exact />
           <Route path='/product/:id' component={ProductScreen}  />
           <Route path='/cart/:id?' component={CartScreen}  />
         </Container>
