5805a3a6907d784d4f84fd0ce948132abb23637a
Swaraj1201 <swarajbhalerao@gmail.com>
2021-04-24 15:21:28 +0530

    Frontend user Authentication added
    Frontend user Authentication added
    
    Co-Authored-By: Abhishek-701 <78553186+Abhishek-701@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,10 @@ import Footer from './Components/Footer'
 import HomeScreen from './Screens/HomeScreen'
 import ProductScreen from './Screens/ProductScreen'
 import CartScreen from './Screens/CartScreen'
+import LoginScreen from './Screens/LoginScreen'
+import RegisterScreen from './Screens/RegisterScreen'
+import ProfileScreen from './Screens/ProfileScreen'
+
 
 
 function App() {
@@ -14,6 +18,9 @@ function App() {
       <main className="py-3">
         <Container >
           <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen}  />
           <Route path='/cart/:id?' component={CartScreen}  />
         </Container>
