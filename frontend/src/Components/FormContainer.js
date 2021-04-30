b01047110225c34a13ab487376dd230effe6a54d
Swaraj1201 <swarajbhalerao@gmail.com>
2021-04-30 13:14:01 +0530

    latest updates
    Co-Authored-By: Abhishek-701 <78553186+Abhishek-701@users.noreply.github.com>

--- frontend/src/Components/FormContainer.js
+++ frontend/src/Components/FormContainer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 
-<<<<<<< HEAD
 function FormContainer({ children }) {
     return (
         <Container>
@@ -10,24 +9,8 @@ function FormContainer({ children }) {
                     {children}
                 </Col>
             </Row>
-=======
-function FormContainer({children}) {
-    return (
-        <Container>
-            <Row className="justify-content-mad-center">
-                <Col xs={12} md={6}>
-                    {children}
-                </Col>
-
-            </Row>
-            
->>>>>>> df338e906fd0705a5802517d52f96f655d7ed059
         </Container>
     )
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> df338e906fd0705a5802517d52f96f655d7ed059
 export default FormContainer
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,4 +1,8 @@
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT } from '../constants/userConstants'
+import { 
+    USER_LOGIN_REQUEST, 
+    USER_LOGIN_SUCCESS, 
+    USER_LOGIN_FAIL, 
+    USER_LOGOUT } from '../constants/userConstants'
 import axios from 'axios'
 
 
@@ -36,4 +40,10 @@ export const login = (email, password) => async (dispatch) => {
             : error.message,
         })
     }
+}
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem('userInfo')
+    dispatch({ type: USER_LOGOUT })
+
 }
